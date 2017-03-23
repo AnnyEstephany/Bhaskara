2b46c6b9582126903bbec7c081ae9b8f15f7396c
anny.ernascimento <annyestephany@hotmail.com>
2017-03-23 20:32:38 -0300

    Solução da Bhaskara as Avessas usando NetDouble

--- src/bhaskara/Bhaskara.java
+++ src/bhaskara/Bhaskara.java
@@ -8,11 +8,11 @@ public class Bhaskara {
         //entrada
         Scanner console = new Scanner(System.in);
         System.out.print(" Digite x1: ");
-        double x1 = console.nextDouble();
+        double x1 = Double.parseDouble(console.nextLine());
         System.out.print(" Digite x2: ");
-        double x2 = console.nextDouble();
+        double x2 = Double.parseDouble(console.nextLine());
         System.out.print(" Digite c: ");
-        double c = console.nextDouble();
+        double c = Double.parseDouble(console.nextLine());
         // processamento
         double s = x1 + x2 ;
         double p = x1 * x2;
